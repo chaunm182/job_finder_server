9dcbc5fe6d64b75029cc4a27e97358aeb8d8b714
Minh Chau <minhchauptit@outlook.com>
2020-06-26 22:46:12 +0700

    change

--- src/main/java/com/example/jobfinder/repository/PostRepository.java
+++ src/main/java/com/example/jobfinder/repository/PostRepository.java
@@ -1,12 +1,10 @@
 package com.example.jobfinder.repository;
 
 import com.example.jobfinder.entity.post.Post;
-import javafx.geometry.Pos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post,Integer> {
 
